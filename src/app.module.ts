b890e974f8348bea70a4d63f0ad611e552e70b97
Salajan Adrian-Antonio <salajanadrian-antonio@Salajan-Adrian-Antonios-MacBook-Pro.local>
2024-07-25 15:18:15 +0300

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'rxjs';
+import * as process from 'node:process';
+import { configuration } from './public/configuration';
+import { validate } from 'class-validator';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { User } from './users/entities/user.entity';
+
+const ENTITIES = [ User ];
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath: [ `config/${process.env.NODE_ENV || "development"}.env`, '.env'],
+    load: [configuration],
+    isGlobal: true,
+    cache: true,
+    validate,
+  }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (config: ConfigService<ConfigModule>) => {
+        return {
+          type: "postgres",
+          host: config.get("DB_HOST"),
+          port: config.get("DB_PORT"),
+          username: config.get("DB_USERNAME"),
+          password: config.get("DB_PASSWORD"),
+          database: config.get("DB_DATABASE"),
+          entities: ENTITIES,
+          synchronize: true,
+          namingStrategy: new SnakeNamingStrategy(),
+        }
+      },
+      inject: [ConfigService],
+  })
+    //
+    //
+    //
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { IConfig } from './public/configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      maxAge: 600,
+    }
+  });
+  app.use(helmet());
+  app.use(json({ limit: "30mb" }))
+  app.use(urlencoded({ extended: true, limit: "30mb" }))
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+  const configService: ConfigService<IConfig> = app.get(ConfigService);
+  const port = configService.get("PORT");
+  await app.listen(port);
 }
 bootstrap();
